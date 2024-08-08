cc9154258834d020958b1c0f9ff7174165d12be2
Drew Ogryzek <drew@molg.ai>
2024-08-07 18:44:09 -0700

    Adds simple form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SimpleFormComponent } from "./components/reactive/simple-form/simple-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SimpleFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/reactive/simple-form/simple-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms'
+
+@Component({
+  selector: 'app-simple-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './simple-form.component.html',
+  styleUrl: './simple-form.component.css'
+})
+export class SimpleFormComponent {
+  name = new FormControl('')
+}

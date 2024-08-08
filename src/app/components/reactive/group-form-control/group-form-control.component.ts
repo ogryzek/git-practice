704562724e3f698dd1873ac0959a4b8cd25e408a
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-07 19:43:24 -0700

    group form control

---
+++ src/app/components/reactive/group-form-control/group-form-control.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-group-form-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './group-form-control.component.html',
+  styleUrl: './group-form-control.component.css'
+})
+export class GroupFormControlComponent {
+
+}
